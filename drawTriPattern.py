51a4996d497a30f9c33b071fb29abbc3d39c2e4b
hillarymonge <rc.monge.hillary@leadps.org>
2016-01-07 15:08:24 -0800

    Create drawTriPattern.py

---
+++ drawTriPattern.py
@@ -0,0 +1,12 @@
+import turtle
+
+ def make.triangle(myTurtle):  
+        myTurtle.penup()
+        myTurtle.pendown()
+        myTurtle.goto(0,0)
+        sidecount = 0 
+        while sidecount < 3:  
+                myTurtle.forward(100)
+                myTurtle.right(120)
+                sidecount = sidecount + 1
+def
