e83f6c302aa323b008bdf19b2ffa299683692380
hillarymonge <rc.monge.hillary@leadps.org>
2016-01-07 14:32:35 -0800

    Create drawteefigure.py

---
+++ drawteefigure.py
@@ -0,0 +1,20 @@
+import turtle
+
+def drawTee(myTurtle):
+  myTurtle.forward(200)
+  myTurtle.backward(50)
+  myTurtle.right(90)
+  myTurtle.backward(100)
+  myTurtle.forward(50)
+  myTurtle.right(90)
+  myturtle.forward(150)
+  
+def drawFourTees(myTurtle):
+  count = 0 
+  while count < 4:
+    myTurtle.drawTee()
+    count = count + 1
+  
+pop = turtle.Turtle
+
+pop.drawFourTees()  
