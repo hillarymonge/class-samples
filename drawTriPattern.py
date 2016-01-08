ed032a7122957618a5f1233cabe40e49bf86fb88
hillarymonge <rc.monge.hillary@leadps.org>
2016-01-07 19:04:03 -0800

    Update drawTriPattern.py

--- drawTriPattern.py
+++ drawTriPattern.py
@@ -9,4 +9,39 @@ import turtle
                 myTurtle.forward(100)
                 myTurtle.right(120)
                 sidecount = sidecount + 1
-def
+
+def drawMoreTriangles(myTurtle):
+        count = 0
+        while count < 4:
+                myTurtle.penup()
+                myTurtle.forward(10)
+                myTurtle.pendown()
+                drawTriangle(myTurtle)
+count = count + 1
+
+def drawTriAlligned(myTurtle):
+        count = 0
+        while count < 3:
+                drawMoreTriangles(myTurtle)
+                myTurtle.penup()
+                myTurtle.goto(0,0)
+                myTurtle.pendown()
+
+count = count + 1
+ 
+ myTurtle.penup()
+        myTurtle.goto(0,0)
+        myTurtle.pendown()
+        myTurtle.left(260)
+        number = 0
+        while number < 3:
+                drawMoreTriangles(myTurtle)
+                myTurtle.penup()
+                myTurtle.goto(0,0)
+                number = number + 1
+
+po = turtle.Turtle()
+
+drawTriAlligned(po)
+
+turtle.exitonclick()
