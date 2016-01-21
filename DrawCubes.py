ba4fdcff1f9be473642b06b65309a066afdf5f0e
hillarymonge <rc.monge.hillary@leadps.org>
2016-01-20 18:35:59 -0800

    Create DrawCubes.py

---
+++ DrawCubes.py
@@ -0,0 +1,118 @@
+import turtle
+
+def drawTopRhombus(myTurtle):
+  count = 0
+  while count < 1:
+    myTurtle.fillcolor("black")
+    myTurtle.begin_fill()
+    myTurtle.left(30)
+    myTurtle.forward(20)
+    myTurtle.left(120)
+    myTurtle.forward(20)
+    myTurtle.left(60)
+    myTurtle.forward(20)
+    myTurtle.left(120)
+    myTurtle.forward(20)
+    myTurtle.left(30)
+    myTurtle.end_fill()
+    count = count + 1
+    
+def drawRightRhombus(myTurtle):
+    count = 0
+    while count < 1:
+      myTurtle.fillcolor("dark blue")
+      myTurtle.begin_fill()
+      myTurtle.left(90)
+      myTurtle.forward(20)
+      myTurtle.right(60)
+      myTurtle.forward(20)
+      myTurtle.right(120)
+      myTurtle.forward(20)
+      myTurtle.right(60)
+      myTurtle.forward(20)
+      myTurtle.end_fill()
+      count = count + 1
+      
+def drawLeftRhombus(myTurtle):
+  count = 0 
+  while count < 1:
+    myTurtle.fillcolor("light blue")
+    myTurtle.begin_fill()
+    myTurtle.right(60)
+    myTurtle.forward(20)
+    myTurtle.left(120)
+    myTurtle.forward(20)
+    myTurtle.left(60)
+    myTurtle.forward(20)
+    myTurtle.left(120)
+    myTurtle.forward(20)
+    myTurtle.end_fill()
+    count = count + 1      
+    
+zo = turtle.Turtle()
+
+def make1toprow(myTurtle):
+  count = 0 
+  while count < 4:
+    drawTopRhombus(zo)
+    zo.penup()
+    zo.forward(35)
+    zo.pendown()
+    count = count + 1
+
+# zo 4 rhombus 2nd row
+#zo.goto(17,-30)
+
+
+# zo makes right rhombus
+def make1rightsiderow(myTurtle):
+  count = 0
+  while count < 4:
+    drawRightRhombus(zo)
+    zo.penup()
+    zo.left(150)
+    zo.forward(35)
+    zo.pendown()
+    count = count + 1
+
+
+def make1leftsiderow(myTurtle):
+  count = 0
+  while count < 4:
+    drawLeftRhombus(zo)
+    zo.penup()
+    zo.right(90)
+    zo.forward(35)
+    zo.right(150)
+    zo.pendown()
+    count = count + 1
+
+zo.speed(0)
+make1toprow(zo)
+zo.penup()
+zo.goto(0,-20)
+zo.pendown()
+make1rightsiderow(zo)
+zo.penup()
+zo.goto(0,0)
+zo.right(150)
+zo.pendown()
+make1leftsiderow(zo)
+zo.penup()
+zo.goto(18,10)
+zo.right(210)
+zo.pendown()
+make1rightsiderow(zo)
+zo.penup()
+zo.goto(18,30)
+zo.pendown()
+make1toprow(zo)
+zo.penup()
+zo.goto(18,30)
+zo.right(150)
+zo.pendown()
+make1leftsiderow(zo)
+zo.penup()
+zo.goto(37,40)
+zo.right(210)
+make1rightsiderow(zo)
