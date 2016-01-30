b898fb07b281b157135de8cbd8eabbff28ec3a74
hillarymonge <rc.monge.hillary@leadps.org>
2016-01-30 13:53:00 -0800

    Update teamManager.py

--- teamManager.py
+++ teamManager.py
@@ -10,24 +10,44 @@ class Player(object):
     print ("age:" + self.age)
     print ("goals: " + self.goals)
     
-  myPlayers = []  
+myPlayers = []  
   
 print ("Hello, what would you like yo do? ")  
 print ("Press (1) to add a player")
 print ("Press (2) to print all players")
-print ("Press (0) to exit")
+print ("Press (3) to exit")
 
 choice = input()
 
-while choice != "0":
-  
-  if choice == "1":
-    print("You picked add a player")
-    print("Name:")
-    name = input()
-    print("Age:")
-    age = input()
-    print("Goals:")
-    goals = input()
+while choice != "3":
     
-    myPlayers.append(Player(name, age, goals))  
+    
+    if choice == "1":
+        print("You picked add a player")
+        print("Name:")
+        name = input()
+        print("Age:")
+        age = input()
+        print("Goals:")
+        goals = input()
+    
+        myPlayers.append(Player(name, age, goals))    
+    
+        print("Your player has been created, what would you like to do now")
+        print ("Press (1) to add a player")
+        print ("Press (2) to print all players")
+        print ("Press (3) to exit")
+        option = input()
+    
+        if option == "2":
+            print("okay, here are all the players:")
+              
+            for information in myPlayers:
+                information.printStats()
+        
+                print("Okay, what do you want now:")
+                print ("Press (1) to add a player")
+                print ("Press (2) to print all players")
+                print ("Press (3) to exit")
+      
+            option = input()
