d4a2acb46c8b84064f1bd944fcfef25fb1c972b2
hillarymonge <rc.monge.hillary@leadps.org>
2016-05-09 15:44:32 -0700

    Update TeamManagerDeluxe.py

--- TeamManagerDeluxe.py
+++ TeamManagerDeluxe.py
@@ -15,7 +15,7 @@ class Player(object):
 		print("Goals: " + str(self.goals))
 		print("Jersey Number: " + str(self.jereseynumber))
 		print("Position: " + self.position)
-		print(\n)
+		print("\n")
 	
 # when user opens a previosly exsisting file.
 def saveTeam(list, filename):
@@ -24,15 +24,15 @@ def saveTeam(list, filename):
 	# write to the file 
 	for p in nlist:
 		file.write(p.name + " "
-		+ str(p.age) + " " 
-		+ str(p.goals) + " " 
-		+ str(p.jerseynumber) + " " 
-		+ p.position + '\n')
+			+ str(p.age) + " " 
+			+ str(p.goals) + " " 
+			+ str(p.jerseynumber) + " " 
+			+ p.position + '\n')
 	# close the file
 	file.close()		
 
 #loads the team, and adds to the list 
-def loadTeam(list, filename):
+def loadTeam(filename):
 	# empty list
 	list  = []
 	# open
@@ -66,19 +66,11 @@ if user == "2":
 	
 else:
 	list = []
-
-# asks user to type in number For option
-
-print("Enter the number of your choice.") 
-print("(1) Make Player") 
-print("(2) List Players") 
-print("(3) Save Players") 
-print("(0) Exit")
  
 # asks user for input
 user= raw_input()
 
-while userchoice != "0":
+while user != "0":
 	print("Enter the number of your choice.") 
 	print("(1) Make Player") 
 	print("(2) List Players") 
@@ -86,7 +78,7 @@ while userchoice != "0":
 	print("(0) Exit (save to avoid losing your team)")
 	user = int(raw_input())
 	
-	if userchoice == "1":
+	if user == "1":
 # user creates a player
 		print("_____________________________________")
 		print("Enter FIRST Name: ")
@@ -110,7 +102,7 @@ while userchoice != "0":
 		print("_____________________________________")
 
 # if 2, will print the player's stats -- name, age etc
-	if userchoice == "2":
+	if user == "2":
 		print("-------------------------------------")
 		print("Current List:")
 		for p in list:
@@ -119,10 +111,10 @@ while userchoice != "0":
 
 # if user input equals 3
 # will add the new list to the loaded list and save to the same file to load later
-	if userchoice == "3":
+	if user == "3":
 		print("What will your file be called? (include .tmd)")
 		filename = raw_input()	
 		saveTeam(list, filename)
 		
-if userchoice == "0":
+if user == "0":
 	print("Bye")	
