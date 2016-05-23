a4679e647b6ab9e6563e2298192a27374389d875
hillarymonge <rc.monge.hillary@leadps.org>
2016-05-23 15:12:54 -0700

    Update primeListerTemplate.py

--- primeListerTemplate.py
+++ primeListerTemplate.py
@@ -1,19 +1,20 @@
-	for n in range(2,my_num):
-		if my_num % n == 0:
-			return False
-	return True
+def isPrime(my_num):
+        for n in range(2,my_num):
+                if my_num % n == 0:
+                        return False
+        return True
 print ("this is true")
 print (isPrime(11))
 print ("this is false")
 print (isPrime(10))
 listt = []
 for number in range(2,10001):
-	y = isPrime(number)
-	if y == True:
-		listt.append(number)
-		myfile = open("primes.txt", "w")
-		myfile.write(str(listt) + "/n")
-		
+        y = isPrime(number)
+        if y == True:
+                listt.append(number)
+                myfile = open("primes.txt", "w")
+                myfile.write(str(listt) + "/n")
+
 print(listt)
 
 myfile.close()
